6db867d5e11ca1f43a1addbd3d485d4917f99f9a
Robert Stephan <robert.stephan@uni-rostock.de>
2024-11-03 13:50:37 +0100

    update Solr API

--- src/main/resources/archetype-resources/__rootArtifactId__-module/src/main/java/__packageInPathFormat__/servlets/ShowMCRObjectsWithFilesServlet.java
+++ src/main/resources/archetype-resources/__rootArtifactId__-module/src/main/java/__packageInPathFormat__/servlets/ShowMCRObjectsWithFilesServlet.java
@@ -1,4 +1,4 @@
-package ${package}.servlets;
+package org.sample.mycore.mycore_sample_1.servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -16,62 +16,63 @@ import org.apache.solr.client.solrj.SolrQuery.ORDER;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
-import org.mycore.solr.MCRSolrClientFactory;
+import org.mycore.solr.MCRSolrCoreManager;
 
 @WebServlet(urlPatterns = "/show-objects-with-files")
 public class ShowMCRObjectsWithFilesServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+    private static final long serialVersionUID = 1L;
 
-	private static Logger LOGGER = LogManager.getLogger();
+    private static Logger LOGGER = LogManager.getLogger();
 
-	@Override
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.setCharacterEncoding("UTF-8");
-		PrintWriter pw = resp.getWriter();
-		pw.println("<html>");
-		pw.println("  <head>");
-		pw.println("    <meta charset=\"utf-8\">");
-		pw.println("    <title>${projectName}: MyCoRe Objekte mit Dateianhang (Derivaten)</title>");
-		pw.println("  </head>");
-		pw.println("  <body>");
-		pw.println("    <h2>${projectName}: MyCoRe Objekte mit Dateianhang (Derivaten)</h2>");
-		outputData(pw);
-		pw.println("  </body>");
-		pw.println("</html>");
-	}
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setCharacterEncoding("UTF-8");
+        PrintWriter pw = resp.getWriter();
+        pw.println("<html>");
+        pw.println("  <head>");
+        pw.println("    <meta charset=\"utf-8\">");
+        pw.println("    <title>${projectName}: MyCoRe Objekte mit Dateianhang (Derivaten)</title>");
+        pw.println("  </head>");
+        pw.println("  <body>");
+        pw.println("    <h2>${projectName}: MyCoRe Objekte mit Dateianhang (Derivaten)</h2>");
+        outputData(pw);
+        pw.println("  </body>");
+        pw.println("</html>");
+    }
 
-	private void outputData(PrintWriter pw) {
+    private void outputData(PrintWriter pw) {
 
-		try {
-			final SolrQuery query = new SolrQuery();
-			query.setStart(0);
-			query.setRows(100);
-			query.setSort("modified", ORDER.desc);
-			query.setQuery("objectType:simpledoc");
+        try {
+            final SolrQuery query = new SolrQuery();
+            query.setStart(0);
+            query.setRows(100);
+            query.setSort("modified", ORDER.desc);
+            query.setQuery("objectType:simpledoc");
 
-			final QueryResponse queryResponse = MCRSolrClientFactory.getMainSolrClient().query(query);
+            final QueryResponse queryResponse = MCRSolrCoreManager.getMainSolrClient().query(query);
 
-			pw.println("<p>" + queryResponse.getResults().getNumFound() + " Objekte im Repository.</p>");
-			if (queryResponse.getResults().getNumFound() > 0) {
-				pw.println("    <table>");
-				pw.println("      <tr><th>MCRID</th><th>zuletzt geändert</th><th>enthält Dateien</th></tr>");
-				for (SolrDocument doc : queryResponse.getResults()) {
-					pw.print("      <tr>");
-					pw.print("<td>" + doc.getFirstValue("id") + "</td>");
-					pw.print("<td>von " + doc.getFirstValue("modifiedby"));
-					pw.print(" am " + doc.getFirstValue("modified") + "</td>");
-					pw.print("<td>" + ("true".equals(doc.getFirstValue("${rootArtifactId}.hasFiles").toString()) ? "ja" : "nein")
-							+ "</td>");
-					pw.println("      </tr>");
-				}
-				pw.println("    </table>");
-			}
-		} catch (SolrServerException | IOException e) {
-			LOGGER.error("Error processing Solr query", e);
+            pw.println("<p>" + queryResponse.getResults().getNumFound() + " Objekte im Repository.</p>");
+            if (queryResponse.getResults().getNumFound() > 0) {
+                pw.println("    <table>");
+                pw.println("      <tr><th>MCRID</th><th>zuletzt geändert</th><th>enthält Dateien</th></tr>");
+                for (SolrDocument doc : queryResponse.getResults()) {
+                    pw.print("      <tr>");
+                    pw.print("<td>" + doc.getFirstValue("id") + "</td>");
+                    pw.print("<td>von " + doc.getFirstValue("modifiedby"));
+                    pw.print(" am " + doc.getFirstValue("modified") + "</td>");
+                    pw.print("<td>"
+                        + ("true".equals(doc.getFirstValue("${rootArtifactId}.hasFiles").toString()) ? "ja" : "nein")
+                        + "</td>");
+                    pw.println("      </tr>");
+                }
+                pw.println("    </table>");
+            }
+        } catch (SolrServerException | IOException e) {
+            LOGGER.error("Error processing Solr query", e);
 
-		}
+        }
 
-	}
+    }
 
 }
